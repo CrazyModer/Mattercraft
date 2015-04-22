b125fe0c17688a15d1ca4608b9275152a0ba1c21
daniel-freak-main <DanielK@Rolands-MBP.fritz.box>
2015-04-22 21:18:46 +0200

    energy shit

--- main/java/net/crazymoder/mattercraft/TileEntity/EnergyPowerCoreTile.java
+++ main/java/net/crazymoder/mattercraft/TileEntity/EnergyPowerCoreTile.java
@@ -2,6 +2,10 @@ package net.crazymoder.mattercraft.tileentity;
 
 import java.util.List;
 
+import cofh.api.energy.EnergyStorage;
+import cofh.api.energy.IEnergyHandler;
+import cofh.api.energy.IEnergyProvider;
+
 import com.sun.javafx.geom.Vec3d;
 
 import net.minecraft.block.Block;
@@ -9,19 +13,73 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
-public class EnergyPowerCoreTile extends TileEntity{
-
+public class EnergyPowerCoreTile extends TileEntity implements IEnergyHandler{
+	
+	private boolean mbsOK;
+	
 	public EnergyPowerCoreTile() {
 		super();
+		mbsOK = false;
 	}
 	@Override
 	public void updateEntity() {
 		super.updateEntity();
+		mbsOK = false;
 		World world = this.worldObj;
 		Block b1 = world.getBlock(xCoord, yCoord+1, zCoord);
 		if(b1.getUnlocalizedName().equals("tile.normalBlock")){
-				System.out.println("Y");
+				mbsOK = true;
 		}
+	}
+	protected EnergyStorage storage = new EnergyStorage(32000);
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+
+		super.readFromNBT(nbt);
+		storage.readFromNBT(nbt);
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbt) {
+
+		super.writeToNBT(nbt);
+		storage.writeToNBT(nbt);
+	}
+
+	/* IEnergyConnection */
+	@Override
+	public boolean canConnectEnergy(ForgeDirection from) {
+
+		return true;
+	}
+
+	/* IEnergyReceiver */
+	@Override
+	public int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate) {
+
+		return storage.receiveEnergy(maxReceive, simulate);
+	}
+
+	/* IEnergyProvider */
+	@Override
+	public int extractEnergy(ForgeDirection from, int maxExtract, boolean simulate) {
+
+		return storage.extractEnergy(maxExtract, simulate);
+	}
+
+	/* IEnergyReceiver and IEnergyProvider */
+	@Override
+	public int getEnergyStored(ForgeDirection from) {
+
+		return storage.getEnergyStored();
+	}
+
+	@Override
+	public int getMaxEnergyStored(ForgeDirection from) {
+
+		return storage.getMaxEnergyStored();
 	}
 }
