2f127a35888ca5864428d1a0b3447b61557e4818
Zarbator <silvermc23@gmail.com>
2015-04-22 21:01:18 +0200

    Ground Plating

--- main/java/net/crazymoder/mattercraft/Manager/BlockManager.java
+++ main/java/net/crazymoder/mattercraft/Manager/BlockManager.java
@@ -3,7 +3,7 @@ package net.crazymoder.mattercraft.manager;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.crazymoder.mattercraft.blockcontainer.EnergyPowerCore;
 import net.crazymoder.mattercraft.blockcontainer.NormalBlock;
-import net.crazymoder.mattercraft.blocks.GenericBlock;
+import net.crazymoder.mattercraft.blocks.GroundPlating;
 import net.crazymoder.mattercraft.tileentity.EnergyPowerCoreTile;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -13,21 +13,26 @@ public class BlockManager {
 	//declarate Blocks
 	public static Block energyPowerCore;
 	public static Block normalBlock;
+	public static Block groundPlating;
 	
 	public BlockManager(){
 		//Initialize
 		energyPowerCore = new EnergyPowerCore(Material.ground);
 		normalBlock = new NormalBlock(Material.ground);
+		groundPlating = new GroundPlating(Material.ground);
 		
 		//Configure
 		energyPowerCore.setHardness(0.5F).setStepSound(Block.soundTypeMetal).setBlockName("energyPowerCore").setCreativeTab(CreativeTabs.tabBlock).setHarvestLevel("pickaxe",0);
 		normalBlock.setHardness(0.5F).setStepSound(Block.soundTypeMetal).setBlockName("normalBlock").setCreativeTab(CreativeTabs.tabBlock).setHarvestLevel("pickaxe",0);
+		groundPlating.setHardness(0.5F).setStepSound(Block.soundTypeMetal).setBlockName("groundPlating").setCreativeTab(CreativeTabs.tabBlock).setHarvestLevel("pickaxe",0);
 		//Textures
 		energyPowerCore.setBlockTextureName("mattercraft:energyPowerCore");
 		normalBlock.setBlockTextureName("mattercraft:normalBlock");
+		groundPlating.setBlockTextureName("mattercraft:groundPlating");
 		//Register
 		GameRegistry.registerBlock(energyPowerCore, "energyPowerCore");
 		GameRegistry.registerBlock(normalBlock, "normalBlock");
+		GameRegistry.registerBlock(groundPlating, "groundPlating");
 	}
 }
 
--- main/java/net/crazymoder/mattercraft/blocks/GroundPlating.java
+++ main/java/net/crazymoder/mattercraft/blocks/GroundPlating.java
@@ -3,10 +3,10 @@ package net.crazymoder.mattercraft.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
-public class GenericBlock extends Block
+public class GroundPlating extends Block
 {
 
-        public GenericBlock (Material material)
+        public GroundPlating (Material material)
         {
                 super(material);
         }
