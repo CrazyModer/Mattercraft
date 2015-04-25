e3606ee438021192c76c58f5f1a2fca9e801b1fd
daniel-freak-main <DanielK@Rolands-MBP.fritz.box>
2015-04-25 21:11:48 +0200

    all controlls

--- main/java/net/crazymoder/mattercraft/manager/TileEntityRenderingManager.java
+++ main/java/net/crazymoder/mattercraft/manager/TileEntityRenderingManager.java
@@ -2,9 +2,11 @@ package net.crazymoder.mattercraft.manager;
 
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.crazymoder.mattercraft.tileentity.CryotheumAcceptorTile;
+import net.crazymoder.mattercraft.tileentity.ReactorTerminalTile;
+import net.crazymoder.mattercraft.tileentity.renderer.ReactorTermianlTileRenderer;
 
 public class TileEntityRenderingManager {
 	public TileEntityRenderingManager(){
-		
+		ClientRegistry.bindTileEntitySpecialRenderer(ReactorTerminalTile.class, new ReactorTermianlTileRenderer());
 	}
 }
