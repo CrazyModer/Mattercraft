6b6e9ca932025728f86a4e89c3be07d9b9475789
daniel-freak-main <DanielK@Rolands-MBP.fritz.box>
2015-04-25 20:43:01 +0200

    fixed

--- main/java/net/crazymoder/mattercraft/manager/FluidManager.java
+++ main/java/net/crazymoder/mattercraft/manager/FluidManager.java
@@ -1,27 +1,12 @@
 package net.crazymoder.mattercraft.manager;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.crazymoder.mattercraft.blocks.BasicFluidBlock;
 import net.crazymoder.mattercraft.fluids.HeatedCryotheum;
 import net.crazymoder.mattercraft.fluids.Hydrogen;
 import net.crazymoder.mattercraft.fluids.IonizedPlasma;
-import net.crazymoder.mattercraft.fluids.LiquidStabilizer;
 import net.crazymoder.mattercraft.fluids.LiquidMatter;
-import net.crazymoder.mattercraft.fluids.Stabilizer;
+import net.crazymoder.mattercraft.fluids.LiquidStabilizer;
 import net.crazymoder.mattercraft.fluids.ToxicWaste;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBucket;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidRegistry;
 
 public class FluidManager {
 	public FluidManager(){
