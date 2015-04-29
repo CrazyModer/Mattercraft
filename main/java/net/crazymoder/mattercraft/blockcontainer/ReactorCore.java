ec547f1848004864a84782c0d4435a12265fd784
daniel-freak-main <DanielK@Rolands-MBP.fritz.box>
2015-04-29 17:57:45 +0200

    x

--- main/java/net/crazymoder/mattercraft/blockcontainer/ReactorCore.java
+++ main/java/net/crazymoder/mattercraft/blockcontainer/ReactorCore.java
@@ -1,10 +1,14 @@
 package net.crazymoder.mattercraft.blockcontainer;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.crazymoder.mattercraft.tileentity.ReactorCoreTile;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class ReactorCore extends BlockContainer{
 
@@ -15,6 +19,11 @@ public class ReactorCore extends BlockContainer{
 	@Override
 	public TileEntity createNewTileEntity(World world, int arg1) {
 		return new ReactorCoreTile();
+	}
+	
+	@SideOnly(Side.CLIENT)
+	public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int side) {
+		return true;
 	}
 
 }
--- main/java/net/crazymoder/mattercraft/tileentity/renderer/ReactorCoreTileRenderer.java
+++ main/java/net/crazymoder/mattercraft/tileentity/renderer/ReactorCoreTileRenderer.java
@@ -22,10 +22,10 @@ public class ReactorCoreTileRenderer extends TileEntitySpecialRenderer{
 
 	private static final ResourceLocation iner_model_texture = new ResourceLocation("mattercraft", "textures/models/power_sphere_layer_1.png");
 	private static final ResourceLocation outer_model_texture = new ResourceLocation("mattercraft", "textures/models/power_sphere_layer_2.png");
-	private IModelCustom iner_model;
+	private IModelCustom innercore;
 
 	public ReactorCoreTileRenderer(){
-		iner_model = AdvancedModelLoader.loadModel(new ResourceLocation("mattercraft", "models/x.obj"));
+		innercore = AdvancedModelLoader.loadModel(new ResourceLocation("mattercraft", "models/core1.obj"));
 	}
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float timeSinceLastTick) {
@@ -46,34 +46,30 @@ public class ReactorCoreTileRenderer extends TileEntitySpecialRenderer{
 		FMLClientHandler.instance().getClient().getTextureManager().bindTexture(iner_model_texture);
 
 
-		//float colour = (float)(Minecraft.getSystemTime()%10000) / 10000f;
-		//double colour = Math.sin((float)Minecraft.getSystemTime() / 10000f);
 		double colour = Math.sin((float)Minecraft.getSystemTime() / 10000f);
 		float brightness = (float)Math.abs(Math.sin((float) Minecraft.getSystemTime() / 3000f) * 100f);
 
 		colour = 1f - colour;
-		//LogHelper.info(colour + " " + ((TileEnergyStorageCore) tile).getEnergyStored() +" "+ ((TileEnergyStorageCore) tile).getMaxEnergyStored());
-		//GL11.glColor4d(1F, 0.3F, 0.7f, 1F);
 		GL11.glScalef(scale, scale, scale);
 		GL11.glPushMatrix();
 		GL11.glRotatef(rotation, 0F, 1F, 0.5F);
 		GL11.glColor4d(1F, colour * 0.3f, colour * 0.7f, 1F);
 		OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 80f + brightness, 80f + brightness);
-		iner_model.renderAll();
+		innercore.renderAll();
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
 		GL11.glScalef(0.8F, 0.8F, 0.8F);
 		GL11.glColor4d(1F, 1f, 0.2f, 1F);
 		GL11.glRotatef(rotation, 0F, 1F, 0.5F);
-		iner_model.renderAll();
+		innercore.renderAll();
 		GL11.glPopMatrix();
 
 		GL11.glPushMatrix();
 		GL11.glScalef(0.9F, 0.9F, 0.9F);
 		GL11.glColor4d(1F, 0f, 0.2f, 1F);
 		GL11.glRotatef(rotation, 0F, 1F, 0.5F);
-		iner_model.renderAll();
+		innercore.renderAll();
 		GL11.glPopMatrix();
 
 		GL11.glScalef(1.1F, 1.1F, 1.1F);
@@ -84,7 +80,7 @@ public class ReactorCoreTileRenderer extends TileEntitySpecialRenderer{
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 		GL11.glRotatef(rotation*0.5F, 0F, -1F, -0.5F);
 		GL11.glColor4f(0.5F, 2F, 2F, 0.7F);
-		iner_model.renderAll();
+		innercore.renderAll();
 		GL11.glDisable(GL11.GL_BLEND);
 		GL11.glDepthMask(true);
 
