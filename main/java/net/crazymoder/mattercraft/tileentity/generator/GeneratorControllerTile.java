3b7d507a5efa1a2d31a3ccb06367366dd857146e
daniel-freak-main <DanielK@Daniels-MBP.fritz.box>
2015-05-09 19:01:53 +0200

    alitlebitblocks

--- main/java/net/crazymoder/mattercraft/tileentity/generator/GeneratorControllerTile.java
+++ main/java/net/crazymoder/mattercraft/tileentity/generator/GeneratorControllerTile.java
@@ -1,16 +1,17 @@
 package net.crazymoder.mattercraft.tileentity.generator;
 
+import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
 
 public class GeneratorControllerTile extends TileEntity{
 	private boolean init = true;
-	private int x,y,z;
+	private int a,b,y,x,z;
 	private int direction;
 	private GeneratorEnergyPortTile energyPortTile;
 	private GeneratorFluidPortTile fluidPortTile1;
 	private GeneratorFluidPortTile fluidPortTile2;
 	public GeneratorControllerTile(){
-		
+		a=0;b=0;y=0;x=0;z=0;
 	}
 	
 	@Override
@@ -28,10 +29,31 @@ public class GeneratorControllerTile extends TileEntity{
 	
 	
 	private boolean checkMbs(){
-		boolean OK = true;
 		System.out.println(xCoord+" "+yCoord+" "+zCoord+" "+direction);
 		
-		return OK;
+		isBlock("tile.air");
+		return true;
 	}
 	
+	private boolean isBlock(String name){
+		if(direction == 2){
+			x = -a;
+			z = b;
+		}
+		if(direction == 4){
+			x = b;
+			z = a;
+		}
+		if(direction == 5){
+			x = -b;
+			z = a;
+		}
+		if(direction == 3){
+			x = a;
+			z = -b;
+		}
+		Block block = worldObj.getBlock(x + xCoord, y + yCoord, z + zCoord);
+		System.out.println(block.getUnlocalizedName());
+		return(block.getUnlocalizedName().equals(name));
+	}
 }
