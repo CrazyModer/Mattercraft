401d3937d02451273e54a8fcad958fe574e20733
Zarantul <ultraakiller@googlemail.com>
2015-05-09 19:37:42 +0200

    SESDRZ

--- main/java/net/crazymoder/mattercraft/manager/WorldGenManager.java
+++ main/java/net/crazymoder/mattercraft/manager/WorldGenManager.java
@@ -1,7 +1,21 @@
 package net.crazymoder.mattercraft.manager;
 
-public class WorldGenManager {
+import java.util.Random;
+
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class WorldGenManager implements IWorldGenerator {
+	
 	public WorldGenManager(){
+		
+	}
+
+	@Override
+	public void generate(Random arg0, int arg1, int arg2, World arg3,
+			IChunkProvider arg4, IChunkProvider arg5) {
+		// TODO Auto-generated method stub
 		
 	}
 }
