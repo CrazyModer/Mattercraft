2256f06ef861198381b252031d656ce6dea98837
Zarantul <ultraakiller@googlemail.com>
2015-05-10 12:17:25 +0200

    1se4wrquosdjsdjidieieieieiieieuideiwui

--- main/java/net/crazymoder/mattercraft/manager/WorldGenManager.java
+++ main/java/net/crazymoder/mattercraft/manager/WorldGenManager.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import cpw.mods.fml.common.IWorldGenerator;
 
 public class WorldGenManager implements IWorldGenerator {
@@ -13,9 +14,34 @@ public class WorldGenManager implements IWorldGenerator {
 	}
 
 	@Override
-	public void generate(Random arg0, int arg1, int arg2, World arg3,
-			IChunkProvider arg4, IChunkProvider arg5) {
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+		switch(world.provider.dimensionId){
+		case -1:
+		    generateNether(world, random, chunkX * 16, chunkZ * 16);
+		    break;
+		case 0:
+		    generateSurface(world, random, chunkX * 16, chunkZ * 16);
+		    break;
+		case 1:
+		    generateEnd(world, random, chunkX * 16, chunkZ * 16);
+		    break;
+		}
+	}
+
+	private void generateEnd(World world, Random random, int i, int j) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	private void generateSurface(World world, Random random, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void generateNether(World world, Random random, int i, int j) {
+		int firstBlockXCoord = i + random.nextInt(16);
+    	int firstBlockYCoord = random.nextInt(64);
+    	int firstBlockZCoord = j + random.nextInt(16);
+		new WorldGenMinable(BlockManager.iridiumOre, 2000).generate(world, random, firstBlockXCoord, firstBlockYCoord,firstBlockZCoord);
 	}
 }
